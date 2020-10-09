827762d288baee82e4a1b9adb2e42aada1b95bd3
Yauheni <68643078+Yauhescha@users.noreply.github.com>
2020-10-09 15:09:04 +0300

    Первое задание

---
+++ task1.pas
@@ -0,0 +1,29 @@
+﻿program Project1;
+
+var
+x,i,sum:Integer;
+arr:Array of Integer;
+begin
+sum:=0;
+Writeln('Write count of elements');
+ReadLn(x);
+SetLength(arr,x);
+
+Writeln('Write elements each other');
+for i:=0 to (x-1)  do
+ReadLn(arr[i]);
+
+Writeln('Yours array');
+for i:=0 to x-1  do
+begin
+  Write('arr['+IntToStr(i)+']='+IntToStr(arr[i])+' ');
+  sum:=sum+arr[i];
+end;
+Writeln('');
+
+Writeln('Sum: '+IntToStr(sum));
+
+
+Readln;
+end.
+ 
