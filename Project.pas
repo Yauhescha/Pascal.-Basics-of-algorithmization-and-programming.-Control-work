91199de4bb651558f5a0b2cbf004c88289748323
Yauheni <68643078+Yauhescha@users.noreply.github.com>
2020-10-11 17:26:34 +0300

    task 3

--- Project.pas
+++ Project.pas
@@ -1,62 +1,37 @@
 ﻿type
-  Dictionary1 = array[1..30] of String; 
-  Dictionary2 = array[1..30] of integer; 
+  TMyArray = array[1..2] of Integer; 
 var
-  dict1:Dictionary1;
-  dict2:Dictionary2;
-  str:String;
-  i,countWordIdDictionary:integer;
-  //функция добавления новой буквы в словарь
-  procedure addLetter(temp:String);
+  W,x,y,z:real;
+
+  //процедура получения минимального/максимального. Минимальный элемент идет первым
+  function getMinMax(A:Integer; B:Integer):TMyArray;
+  var arr:TMyArray;
   begin
-    countWordIdDictionary:=countWordIdDictionary+1;
-    dict1[countWordIdDictionary]:=temp;
-    dict2[countWordIdDictionary]:=1;
-  end;
-  //функция увеличения количества использований
-  procedure upgradeLetter(index:Integer);
-  begin
-    dict2[index]:=dict2[index]+1;
+    if(A<B) then 
+    begin
+      arr[1]:=A;
+      arr[2]:=B;
+    end else begin
+      arr[1]:=B;
+      arr[2]:=A;
+    end;
+    Result:=arr;    
   end;
   
-  
-  //попытка записи буквы в словарь
-  procedure writeInDictionary(temp:String);
-  var 
-    isInDict:Boolean;
-    j,currentLetterIndex:Integer;
+  //получение минимума
+  function min(A:Integer; B:Integer):Integer;
   begin
-    isInDict:=false;
-    currentLetterIndex:=-1;
-    for j:=1 to 30 do
-      if dict1[j]=temp then 
-      begin        
-        isInDict:=true;
-        currentLetterIndex:=j;
-      end;
-    if(isInDict=false) then addLetter(temp)
-    else upgradeLetter(currentLetterIndex);
+    Result:=getMinMax(A,B)[0];
   end;
-//печать словаря  
-  procedure pringDictionary();
-  var i:Integer;
+  //получение максимума
+  function max(A:Integer; B:Integer):Integer;
   begin
-    for i:=1 to countWordIdDictionary do
-       Write('"'+dict1[i]+'" ');
-    Writeln();
-    for i:=1 to countWordIdDictionary do
-       Write('"'+dict2[i]+'" ');    
+    Result:=getMinMax(A,B)[1];
   end;
-  
-// точка входа в программу  
-begin
-countWordIdDictionary:=0;
-Writeln('Write yours string');
-Readln(str);
-
-for i:=1 to str.Length do
-  writeInDictionary(str[i]);
-
-pringDictionary();
-
+  begin
+Writeln('Write x');ReadLn(x);
+Writeln('Write y');ReadLn(y);
+Writeln('Write z');ReadLn(z);
+W:=min(x,y)/max(x,y) + max(x,y)/min(y,z+x);
+Writeln('Answer: '+W);
 end.
